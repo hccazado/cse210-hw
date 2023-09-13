1d6af4257c8095374851cc6da4683457288f7bb0
Heitor Cazado <heitorcazado@gmail.com>
2023-09-13 00:15:51 -0300

    Completed prep assignments 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Variables
+        string firstName, lastName;
+
+        Console.WriteLine("What is your first name?");
+        
+        firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        float gradePercentage;
+
+        string letter = "", sign = "";
+
+        Console.WriteLine("Please inform your grade percentage:");
+
+        string input = Console.ReadLine();
+
+        gradePercentage = float.Parse(input);
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //determing sign of letter grade
+        if(gradePercentage%10 >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if(gradePercentage%10 < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine($"Congratulations! You approved with an: {letter}{sign}.");
+        }
+        else{
+            Console.WriteLine($"Sorry, you haven't approved this time. You got an {letter}{sign}.");
+        }
     }
 }
