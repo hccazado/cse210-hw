ae13e1dcef99759526d454d3cce892a8635f65ef
Heitor Cazado <heitorcazado@gmail.com>
2023-09-18 22:33:00 -0300

    started prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //variables
+        int magicNumber = 7;
+        
+        string guess = "";
+
+        bool isPlaying = true;
+
+        do {
+
+            Console.WriteLine("Type your guess: ");
+            
+            guess = Console.ReadLine();
+
+            Console.WriteLine(" you guessed: " + guess);
+
+            if (int.Parse(guess) > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (int.Parse(guess) < magicNumber) {
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("Congrats! You guessed ir right");
+            }
+        
+        } while(isPlaying);
+
+        Console.WriteLine("Bye!!");
     }
 }
