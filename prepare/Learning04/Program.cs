290fcc468260b3280f32b31a85076a4e309e89a2
Heitor Cazado <heitorcazado@gmail.com>
2023-10-24 13:55:14 -0300

    Inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math1 = new MathAssignment("Robberto Rodriguez","Fractions","7.3","8-19");
+        WritingAssigment wrt1 = new WritingAssigment("Mary Waters","European History","The Causes of World War II");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        Console.WriteLine(wrt1.GetWritingInformation());
     }
 }
