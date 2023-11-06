5c6599cee803eb629ab175dcad7bb9cf67066aca
Heitor Cazado <heitorcazado@gmail.com>
2023-11-05 21:29:04 -0300

    learning activity w5 - started

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sq = new Square("red",2);
+
+        Rectangle rt = new Rectangle("blue", 2.3, 3.3);
+
+        Circle cr = new Circle("green", 6.2);
+
+        Console.WriteLine(sq.GetColor());
+        Console.WriteLine(rt.GetColor());
+        Console.WriteLine(cr.GetColor());
+        Console.WriteLine(sq.GetArea());
+        Console.WriteLine(rt.GetArea());
+        Console.WriteLine(cr.GetArea());
+
     }
 }
