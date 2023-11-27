0b526fb86654c76d7128c44d660558a3ad6ad18c
Heitor Cazado <heitorcazado@gmail.com>
2023-11-27 17:58:36 -0300

    address and allergy class completed

--- final/FinalProject/Address.cs
+++ final/FinalProject/Address.cs
@@ -2,4 +2,46 @@ class Address
 {
     private string _address;
     private string _number;
+    private string _zip;
+    private string _city;
+    private string _state;
+
+    public Address (string address, string number, string zip, string city, string state)
+    {
+        _address = address;
+        _number = number;
+        _zip = zip;
+        _city = city;
+        _state = state;
+    }
+
+    public void SetAddress(string address)
+    {
+        _address = address;
+    }
+
+    public void SetNumber(string number)
+    {
+        _number = number;
+    }
+
+    public void SetZip(string zip)
+    {
+        _zip = zip;
+    }
+
+    public void SetCity(string city)
+    {
+        _city = city;
+    }
+
+    public void SetState(string state)
+    {
+        _state = state;
+    }
+
+    public string GetAddress()
+    {
+        return $"{_number},{_address} - {_city}, {_state} - {_zip}";
+    }
 }
--- final/FinalProject/Allergy.cs
+++ final/FinalProject/Allergy.cs
@@ -1,4 +1,61 @@
 class Allergy
 {
-    
+    private string _snomedCtId;
+    private string _icd10;
+    private string _description;
+
+    public Allergy(string description, string snomed = null, string icd=null)
+    {
+        _snomedCtId = snomed;
+        _icd10 = icd;
+        _description = description;
+    }
+
+    public void SetSnomedCtId(string id)
+    {
+        _snomedCtId = id;
+    }
+
+    public void SetDescription(string desc)
+    {
+        _description = desc;
+    }
+
+    public void SetIcd(string icd)
+    {
+        _icd10 = icd;
+    }
+
+    public string GetSnomedCtID()
+    {
+        return _snomedCtId;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public string GetIcd()
+    {
+        return _icd10;
+    }
+
+    public string GetAllergy()
+    {
+        if (_snomedCtId != null && _icd10 != null)
+        {
+            return $"Snomed CT: {_snomedCtId} - ICD10: {_icd10} - Desc: {_description}";
+        }
+
+        else if(_snomedCtId == null)
+        {
+            return $"ICD10: {_icd10} - Desc: {_description}";
+        }
+
+        else
+        {
+            return $"Snomed CT: {_snomedCtId} - Desc: {_description}";
+        }
+    }
 }
